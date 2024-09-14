c9acb33ea49759a86834edc1a3115bebc7db0e3d
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-09-14 15:00:17 +0530

    Rewrote code after accidental removal

--- arrays12.java
+++ arrays12.java
@@ -1,3 +1,4 @@
+//   how to write 2d array code
 import java.util.Scanner;
 public class arrays12{
     static void printarray(int [][] array){
@@ -13,19 +14,22 @@ System.out.print(array[i][j]  +   "   ");
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number of rows  :      ");
         int r = sc.nextInt();
-        System.out.println("enter the number of column :       ");
+        System.out.println("enter the number of column :   ");
         int c = sc.nextInt();
         int [][] array = new int[r][c];
-        System.out.println("enter    "    + r*c +  " elements :   ");
-        for(int i=0; i<r; i++){
-            for(int j =0; j<c; j++){
+        System.out.println("enter "   + r*c  +  "elements :   "
+        );
+        for(int i =0; i<r; i++){
+            for(int j=0; j<c;j ++){
                 array[i][j] = sc.nextInt();
-
             }
         }
+        
+        
+        
+        
         printarray(array);
         
-            // { 1,10,11,99},
-            // {3,90,21} }; printarray(arr2);
+            
     }
 }
--- arrays13.java
+++ arrays13.java
@@ -1,12 +1,32 @@
+//  2d array matrix addition question:
 import java.util.Scanner;
 public class arrays13{
+    static void printarray(int [][] array){
+        for(int i =0; i<array.length; i++){
+            for(int j =0; j<array[i].length; j++){
+System.out.print(array[i][j]  +   "   ");
+            } System.out.println(); 
+        
+        
+        } 
+        } 
+      
+
+    
     static void add(int [][] a ,  int r1,  int c1 , int [][] b , int r2 , int c2 ){
         if(r1!=r2 || c1!=c2){
             System.out.println("addition not possible wrong dimesnion");
-        }
-    }
+        return; } int [][] sum = new int[r1][c1];
+        for(int i =0; i<r1; i++){
+            for(int j=0; j<c1; j ++){
+                sum [i][j] =   a[i][j]  + b[i][j];
+            }
+        } printarray(sum);
+        } 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number of rows:    ");
+        int r1 
 
     }
 }
