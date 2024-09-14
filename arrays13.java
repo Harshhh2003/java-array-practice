55d0d9460827235d20b8e3e2bbd46efc332b1f7a
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-09-14 15:37:15 +0530

    Completed incomplete matrix addition code

--- arrays13.java
+++ arrays13.java
@@ -1,32 +1,66 @@
+
 //  2d array matrix addition question:
 import java.util.Scanner;
-public class arrays13{
-    static void printarray(int [][] array){
-        for(int i =0; i<array.length; i++){
-            for(int j =0; j<array[i].length; j++){
-System.out.print(array[i][j]  +   "   ");
-            } System.out.println(); 
-        
-        
-        } 
-        } 
-      
 
-    
-    static void add(int [][] a ,  int r1,  int c1 , int [][] b , int r2 , int c2 ){
-        if(r1!=r2 || c1!=c2){
-            System.out.println("addition not possible wrong dimesnion");
-        return; } int [][] sum = new int[r1][c1];
-        for(int i =0; i<r1; i++){
-            for(int j=0; j<c1; j ++){
-                sum [i][j] =   a[i][j]  + b[i][j];
+public class arrays13 {
+    static void printarray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "   ");
             }
-        } printarray(sum);
-        } 
+            System.out.println();
+
+        }
+    }
+
+    static void add(int[][] a, int r1, int c1, int[][] b, int r2, int c2) {
+        if (r1 != r2 || c1 != c2) {
+            System.out.println("addition not possible wrong dimesnion");
+            return;
+        }
+        int[][] sum = new int[r1][c1];
+        for (int i = 0; i < r1; i++) {
+            for (int j = 0; j < c1; j++) {
+                sum[i][j] = a[i][j] + b[i][j];
+  
+            }
+        }
+        System.out.println("sum of matrix 1 and matrix 2  is :  ");
+        printarray(sum);
+   
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number of rows:    ");
-        int r1 
+   
+        System.out.println("enter the number of rows  of mat 1 :      ");
+        int r1 = sc.nextInt();
+        System.out.println("enter the number of column of mat 2 :   ");
+        int c1 = sc.nextInt();
+        int [][] a = new int[r1][c1];
+       
+        System.out.print("elements are :  ");
+        for(int i =0; i<r1; i++){
+            for(int j=0; j<c1;j ++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+       System.out.println("enter the row 2 : ");
+        int r2 = sc.nextInt();
+        System.out.println("enter the column 2 ");
+        int c2 = sc.nextInt();
+        int [][] b = new int [r2][c2];
+        System.out.println("elemts are  :  ");
+        for(int i =0; i<r2; i++){
+            for(int j=0; j<c2;j ++){
+                b[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("matrix 1");
+        System.out.println(a);
+        System.out.println("amtrix b"); System.out.println(b);
+        add(a, r1, c1, b, r2, c2);
+        
 
     }
 }
