6db8339e066d669af2ad6f465bcd3ebfeb2f5fd2
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2024-10-25 23:08:17 +0530

    tranpose matrix of 2d array question

---
+++ MatrixTranspose.java
@@ -0,0 +1,30 @@
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        int[][] transpose = new int[cols][rows];
+
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transpose[j][i] = matrix[i][j];
+            }
+        }
+
+        
+        System.out.println("Transpose of the matrix:");
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
